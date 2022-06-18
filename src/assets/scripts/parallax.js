7c60f3ca6d0b0c26a9dbd925744bb181f1fca085
KseniyaYaskevich <yaskevi4kseniya@gmail.com>
2022-06-18 13:34:33 +0300

    feat: add animOnScroll function

--- src/assets/scripts/parallax.js
+++ src/assets/scripts/parallax.js
@@ -12,6 +12,30 @@ const offset = (el) => {
   };
 };
 
+const animOnScroll = () => {
+  for (let index = 0; index < animItems.length; index++) {
+    const animItem = animItems[index];
+    const animItemHeight = animItem.offsetHeight;
+    const animItemOffset = offset(animItem).top;
+    const animStart = animItem.getAttribute('data-anim-start');
+
+    let animItemPoint = window.innerHeight - animItemHeight / animStart;
+
+    if (animItemHeight > window.innerHeight / animStart) {
+      animItemPoint = window.innerHeight - window.innerHeight / animStart;
+    }
+
+    if ((window.pageYOffset > animItemOffset - animItemPoint) && window.pageYOffset < (animItemOffset + animItemHeight)) {
+      animItem.classList.add('_active');
+    } else if (animItem.getBoundingClientRect().top <= 0) {
+      animItem.classList.add('_active');
+    } else {
+      animItem.classList.remove('_active');
+    }
+  }
+};
+
+animOnScroll();
 
 let scrollPosition = 0;
 
@@ -19,3 +43,5 @@ document.addEventListener('scroll', function () {
   scrollPosition = window.pageYOffset;
   parallaxWrapper.style.transform = 'translate3d(0px,' + scrollPosition / 3 + 'px, 0px) scale(' + +(1 + scrollPosition / 2000) + ')';
 });
+
+window.addEventListener('scroll', animOnScroll);
